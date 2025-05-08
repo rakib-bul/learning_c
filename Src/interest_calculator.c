180437ad702123930675b64f24180241bf4e7348
Rakib Hasan Bul Bul <hrakib182@gmail.com>
2025-05-08 09:56:20 +0600

    Update interest_calculator.c

--- Src/interest_calculator.c
+++ Src/interest_calculator.c
@@ -9,21 +9,22 @@ int main(){
     double total = 0.0;
 
     printf("Interest Calculator \n");
+
     printf("Enter the Principal Amount (P):\n");
-    scanf("%0.10lf", &principal);
+    scanf("%lf", &principal);
 
     printf("Enter the interest rate % (R): \n");
-    scanf("%0,10lf", &rate);
+    scanf("%lf", &rate);
     rate = rate/100;
 
     printf("Enter the number of year (t): \n");
     scanf("%d", &years);
 
-    prinf("Enter the number of compound per year (n): 0");
+    printf("Enter the number of compound per year (N): \n");
     scanf("%d", &times_compounded);
 
     total = principal * pow(1 + rate / times_compounded, times_compounded * years);
-    printf("After %d years the total will be %0.10lf BDT \n",years, total);
+    printf("After %d years the total amount will be %0.10lf BDT \n",years, total);
 
 
     return 0;
