bba1070a19da88f7511fedcfafef8b0d06a35f4f
Rakib Hasan Bul Bul <hrakib182@gmail.com>
2025-05-15 17:58:39 +0600

    Create simple_library_manager.c

---
+++ Src/simple_library_manager.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+struct library{
+    char book_name[30];
+    char author_name[30];
+    int pages;
+    char status[10];
+    float price;
+};
+
+int main(){
+
+}
