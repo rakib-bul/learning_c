af7d0fd317d8ed14688a52e581ab499a89a4978d
Rakib Hasan Bul Bul <hrakib182@gmail.com>
2025-07-24 23:11:32 +0600

    Update character_creator.c

--- Hervard CS50/Learning C/character_creator.c
+++ Hervard CS50/Learning C/character_creator.c
@@ -5,14 +5,17 @@
 //Declare Functions
 void character_name();
 void player_classes();
+void player_gender();
 
 // Variables
 char name[48];
 char classes[4][10] = {"Warrior", "Mage", "Archer", "Rogue"};
+char gender[2][10] = {"Male", "Female"};
 
 int main(){
     character_name();
     player_classes();
+    player_gender();
 
     return 0;
 }
@@ -31,10 +34,41 @@ void character_name(){
 
 void player_classes(){
 
-    int choice;
+    int class_choice;
 
     printf("Please choose your class (1/2/3/4) >> \n");
     for (int i = 0; i < 4; i++){
         printf("%d. %s \n", i+1, classes[i]);
     }
+
+    do {
+        printf(">> ");
+        scanf("%d", &class_choice);
+        while (getchar() != '\n');
+    } while (class_choice < 1 || class_choice > 4);
+
+    printf("You have chosen %s. \n", classes[class_choice -1]);
+
+
+
 }
+
+void player_gender(){
+    int gender_choice;
+
+    printf("Choose Gender (1/2) >> \n");
+
+    for (int i = 0; i < 2; i++){
+        printf("%d. %s \n", i+1, gender[i]);
+    }
+
+    do{
+        printf(">> ");
+        scanf("%d", &gender_choice);
+        while (getchar() != '\n');
+    } while (gender_choice < 1 || gender_choice > 2);
+
+    printf("You have chosen %s. \n", gender[gender_choice - 1]);
+}
+
+
