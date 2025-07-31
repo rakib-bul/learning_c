caf5f12b55c7339c4f423f8057a703450503f757
Rakib Hasan Bul Bul <hrakib182@gmail.com>
2025-07-31 17:42:45 +0600

    Create rock_paper_scissors.c

---
+++ Hervard CS50/Learning C/rock_paper_scissors.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<time.h>
+#include<stdlib.h>
+
+void generate_computer_choice();
+int computer_choice = 0;
+int main(){
+
+    char choice[3][10] = {"Rock", "Paper", "Scissor"};
+    int player_input = 0;
+
+
+    printf("Welcome to Rock, Paper, Scissors\n");
+
+    for(int i = 0; i<3; i++){
+        printf("%d. %s\n", i+1 , choice[i]);
+
+    }
+    printf("Enter Your choice >> ");
+
+    scanf("%d", &player_input);
+
+
+
+    if(player_input >= 1 && player_input <= 3){
+        printf("%s\n", choice[player_input -1]);
+    }
+    else{
+        printf("Fuck you");
+        return 0;
+    }
+
+    generate_computer_choice();
+
+    printf("Computer Choice = %s \n", choice[computer_choice -1 ]);
+
+    //Game logic
+
+
+}
+void generate_computer_choice(){
+    srand(time(NULL));
+    computer_choice = rand()%3;
+
+}
