3804a4bbd1d401507652bd52253feb989a1648cd
Rakib Hasan Bul Bul <hrakib182@gmail.com>
2025-08-04 19:25:47 +0600

    distance calculator

--- Hervard CS50/Learning C/bmi_calculator.c
+++ Hervard CS50/Learning C/bmi_calculator.c
@@ -12,18 +12,22 @@ int main(){
 
     printf("BMI Calculator\n");
     printf("Name= ");
-    scanf("%c", name);
+    scanf("%s", name);
 
     printf("Choose Your Gender : \n");
 
     for(int i = 0; i<2; i++){
         printf("%d. %s \n", i+1, gender[i]);
-        scanf("%d", &gender_select;
-    }
-    if(gender_select >=1 && <= gender_select){
 
     }
+    scanf("%d", &gender_select);
 
+    if(gender_select >=1 && gender_select<= 2){
+        printf("%s\n",gender[gender_select-1]);
+    }
+    else{
+        printf("Invalid Gender. Choose again. \n");
+    }
 
 
     printf("Enter your height(ft)= ");
@@ -33,10 +37,31 @@ int main(){
     printf("Enter your weight(kg) = ");
     scanf("%i", &weight);
 
+
+
     height = (((feet*12)+inch)/39.37);
     bmi = weight/(height * height);
 
-    printf("You BMI is %lf",bmi);
+    printf("Your BMI is %0.2f\n",bmi);
+
+    if (bmi < 16)
+        printf("Category: Severely Underweight\n");
+    else if (bmi >= 16 && bmi < 17)
+        printf("Category: Moderately Underweight\n");
+    else if (bmi >= 17 && bmi < 18.5)
+        printf("Category: Mildly Underweight\n");
+    else if (bmi >= 18.5 && bmi < 25)
+        printf("Category: Normal Weight\n");
+    else if (bmi >= 25 && bmi < 30)
+        printf("Category: Overweight\n");
+    else if (bmi >= 30 && bmi < 35)
+        printf("Category: Obese Class I\n");
+    else if (bmi >= 35 && bmi < 40)
+        printf("Category: Obese Class II\n");
+    else
+        printf("Category: Obese Class III\n");
+
+    return 0;
 
 
 }
---
+++ Hervard CS50/Learning C/distance_calc.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    float km;
+    float mile;
+    float km_convert = 0.2;
+    float mile_convert = 0.3;
+    printf("Distance Calculator");
+}
