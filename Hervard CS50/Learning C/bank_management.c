68c07307a43efefdfa68b294f3f85f136433bea8
Rakib Hasan Bul Bul <hrakib182@gmail.com>
2025-08-17 11:32:26 +0600

    Create bank_management.c

---
+++ Hervard CS50/Learning C/bank_management.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+//Varibales
+int account_id;
+char first_name[50];
+char last_name[50];
+char father_name[100];
+char mother_name[100];
+
+int main(){
+}
+
+
+//Create Account
+
+//User
+
+
