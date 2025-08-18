60a83bdbb4e389d626fdc1acd8012145a7f3889f
rakib-bul <hrakib182@gmail.com>
2025-08-18 21:22:06 +0600

    bank

--- Hervard CS50/Learning C/bank_management.c
+++ Hervard CS50/Learning C/bank_management.c
@@ -9,6 +9,11 @@ char first_name[50];
 char last_name[50];
 char father_name[100];
 char mother_name[100];
+char present_address[100];
+char permanent_address[100];
+char address_selector;
+
+
 
 
 //Functions
@@ -39,6 +44,37 @@ void create_account(){
         printf("%d %d \n",i+1, account_id);
 
     }
+
+    printf("Enter First Name: ");
+    scanf("%s", &first_name);
+
+    printf("Enter Last name: ");
+    scanf("%s", &last_name);
+
+    printf("Enter Father's name: ");
+    scanf("%s", &father_name);
+
+    printf("Enter mother's name: ");
+    scanf("%s", &mother_name);
+
+    printf("Present Address: ");
+    scanf("%s", present_address);
+
+    while (getchar() != '\n');
+    
+
+    printf("Is permanent & present address same? y/n >> ");
+    scanf("%c", &address_selector);
+
+    if (address_selector == "y" || "Y"){
+        strcpy(permanent_address,present_address);
+        printf("%s\n", permanent_address);
+    }
+    else{
+        printf("Enter correct value\n");
+    }
+
+
 
 
 }
